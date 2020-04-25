7f803aa3b161428421dc8d26587b3ce8687c73a4
Milos Veljkovic <milosveljkovic@boemskats.com>
2020-04-25 18:01:00 +0200

    small changes

--- services/path_service/db.py
+++ services/path_service/db.py
@@ -7,7 +7,7 @@ DB_LOCALHOST = 'localhost'
 
 FACTOR = 10
 BOTTOM_HEART_RATE = 60
-M_TO_KM=1000
+M_TO_KM = 1000
 
 
 def generate_get_runners_path_query(runner_id):
@@ -24,6 +24,6 @@ def create_insert_path_query(current_x, current_y, current_distance, speed, cur_
     INSERT INTO iot.path (id, runner_id,path_x,path_y,speed,heart_rate,distance,path_name) 
     VALUES
     ( "{uuid.uuid4()}",(SELECT id from iot.runner WHERE id='{cur_email}'),
-    {current_x},{current_y},{speed},{speed*FACTOR+BOTTOM_HEART_RATE},{round(current_distance/M_TO_KM, 2)},"{path_name}");
+    {current_x},{current_y},{speed},{speed * FACTOR + BOTTOM_HEART_RATE},{round(current_distance / M_TO_KM, 2)},"{path_name}");
     """
     return query
